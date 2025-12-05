4a7db9e26aa08460aadc5bb6fcac138296a16f75
Maksim Gurenkov <git@smashingalpha.com>
2025-12-05 15:00:53 +0300

    real fix

--- regresql/tap.go
+++ regresql/tap.go
@@ -24,20 +24,32 @@ func (p *Plan) CompareResultSets(regressDir string, expectedDir string, t *tap.T
 			filepath.Base(rs.Filename))
 		diff, err := DiffFiles(expectedFilename, rs.Filename, 3)
 
+		var names, bindings, file string
+
+		if len(p.Names) == 0 {
+			file = "-"
+			names = "-"
+			bindings = "-"
+		} else {
+			file = p.Path
+			names = p.Names[i]
+			bindings = fmt.Sprintf("%v", p.Bindings[i])
+		}
+
 		if err != nil {
 			t.Diagnostic(
 				fmt.Sprintf(`Query File: '%s'
 Bindings File: '%s'
 Bindings Name: '%s'
-Query Parameters: '%v'
+Query Parameters: '%s'
 Expected Result File: '%s'
 Actual Result File: '%s'
 
 Failed to compare results: %s`,
 					p.Query.Path,
-					p.Path,
-					p.Names[i],
-					p.Bindings[i],
+					file,
+					names,
+					bindings,
 					expectedFilename,
 					rs.Filename,
 					err.Error()))
@@ -48,16 +60,15 @@ Failed to compare results: %s`,
 				fmt.Sprintf(`Query File: '%s'
 Bindings File: '%s'
 Bindings Name: '%s'
-Query Parameters: '%v'
+Query Parameters: '%s'
 Expected Result File: '%s'
 Actual Result File: '%s'
 
 %s`,
 					p.Query.Path,
-					p.Path,
-					//					p.Names[i],
-					//					p.Bindings[i],
-					"<name>", "<binding>",
+					file,
+					names,
+					bindings,
 					expectedFilename,
 					rs.Filename,
 					diff))
